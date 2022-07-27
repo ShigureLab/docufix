0f9716e5f02cade475a3137ce2a58104b2aa7a5c
SigureMo <sigure.qaq@gmail.com>
2022-07-27 17:36:40 +0800

    🔖 release: v0.3.2

--- docufix/__init__.py
+++ docufix/__init__.py
@@ -1,7 +1,7 @@
 # Meta information for the project.
 from __future__ import annotations
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Nyakku Shigure"
 __year__ = "2022"
 __project_info__ = {
